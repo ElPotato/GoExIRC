a83f8aa8363aa1847e792be3bc2d69a2fb5cd115
ElPotato <marcinbieszke@gmail.com>
2021-01-04 13:09:57 +0000

    add terminate option to kill process remotely

--- handler.go
+++ handler.go
@@ -1,12 +1,19 @@
 package main
 
-import irc "gopkg.in/irc.v3"
+import (
+	"os"
+
+	irc "gopkg.in/irc.v3"
+)
 
 func handler(c *irc.Client, m *irc.Message) {
 	switch {
 	// join channel on welcome command 001
 	case m.Command == "001":
 		c.Write("JOIN " + CHANNEL)
+	// terminate process
+	case m.Command == "PRIVMSG" && c.FromChannel(m) && m.Params[1] == "terminate":
+		os.Exit(0)
 	// pass every message from channel to executeCommand / return its output
 	case m.Command == "PRIVMSG" && c.FromChannel(m):
 		c.WriteMessage(&irc.Message{
