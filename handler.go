2b62e475df2f05b09be539e929afaa5fee4b7ea3
ElPotato <marcinbieszke@gmail.com>
2021-01-07 09:04:54 +0000

    move terminate into separated file

--- handler.go
+++ handler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os"
 	irc "gopkg.in/irc.v3"
 )
 
@@ -10,9 +9,9 @@ func handler(c *irc.Client, m *irc.Message) {
 	// join channel on welcome command 001
 	case m.Command == "001":
 		c.Write("JOIN " + CHANNEL)
-	// terminate process
+	// terminate node
 	case m.Command == "PRIVMSG" && c.FromChannel(m) && m.Params[1] == "terminate":
-		os.Exit(0)
+		terminate()
 	// pass every message from channel to executeCommand / return its output
 	case m.Command == "PRIVMSG" && c.FromChannel(m):
 		c.WriteMessage(&irc.Message{
