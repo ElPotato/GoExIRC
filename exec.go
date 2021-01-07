2df1ae08d0e0fee715b06687dc0b96fbc0f1329f
ElPotato <marcinbieszke@gmail.com>
2021-01-07 08:45:03 +0000

    remove fmt debug

--- exec.go
+++ exec.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt" // debug
 	"os/exec"
 	"strings"
 )
@@ -9,9 +8,6 @@ import (
 func executeCommand(input string) string {
 	cmd, params := splitParams(input)
 	out, _ := exec.Command(cmd, params...).Output()
-
-	fmt.Printf("debug executing: %v, %v\n", cmd, params)
-
 	return strings.ReplaceAll(string(out), "\n", " \\n ")
 }
 
